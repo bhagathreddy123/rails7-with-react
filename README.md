a20e35d65ee76cb89238e4aa7fa80b365fb4dfc3
bhagathreddy123 <katta_reddy04@yahoo.com>
2023-10-08 10:00:27 -0700

    implemented Rails api for update question for like and dislike

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ rails new rails7-with-react -j=esbuild
   npm i react react-dom
 gem install foreman
    ./bin/dev
+
+
+in postman checking params
+using post method
+localhost:3000/api/v1/questions/2/update_counter
+
+ "count_for": "dislikes"
+
+in body json params selection passing count_for param based on like and dislike and verified.
--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::QuestionsController < ApplicationController 
+	protect_from_forgery with: :null_session	
 	def index
 		@questions = Question.all
 		render json: @questions, status: :ok
 	end
 
+	def update_counter
+		@question = Question.find(params[:id])
+		if params[:count_for] == "likes"
+			@question.update(likes_count: @question.likes_count + 1)
+		elsif params[:count_for] = "dislikes"
+			@question.update(dislikes_count: @question.dislikes_count + 1)
+
+		end
+		render json: @question, status: :ok
+	end
 end
