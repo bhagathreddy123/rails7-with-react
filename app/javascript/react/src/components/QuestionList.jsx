3dc1292677b70f5b07b7e617128c056bf3a42edf
bhagathreddy123 <katta_reddy04@yahoo.com>
2023-10-08 06:39:07 -0700

    fetching API response in react component

--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -1,37 +1,26 @@
 import * as React from 'react'
+import { useState, useEffect } from 'react'
 import * as ReactDom from 'react-dom'
 import QuestionDetail from './QuestionDetail'
  
 const QuestionList = () => {
-	const questionsList = [
-	{ 
-		id: 1,
-		title: "What is React?",
-		ans: "open-source front-end JavaScript library"
-	},
-	{ 
-		id: 2,
-		title: "What are the major features of React?",
-		ans: "JSX syntax"
-	},
-	{ 
-		id: 3,
-		title: "Why is DevTools not loading in Chrome for local files?",
-		ans: "open Chrome Extensions and check Allow access to file URLs"
-	},
-	{ 
-		id: 4,
-		title: "Why is DevTools not loading in Chrome for local files?",
-		ans: "open Chrome Extensions and check Allow access to file URLs"
-	},
-	{ 
-		id: 5,
-		title: "What are the advantages of React over Vue.js?",
-		ans: "Gives more flexibility in large apps developing"
+	const [questionsList, setQuestionsList] = useState([])
+	const questionsUrl = 'http://localhost:3000/api/v1/questions'
+
+	const fetchQuestionList = () => {
+      fetch(questionsUrl)
+       .then((response) => response.json())
+       .then((data) => {
+       	 console.log(data)
+       	 setQuestionsList(data)
+       })
 	}
 
-	]
+	useEffect(() => {
+		fetchQuestionList()
+	}, [])
 
+	
 	return (
 		<div className="row">
 			<div className="col-lg-10 mx-auto">
