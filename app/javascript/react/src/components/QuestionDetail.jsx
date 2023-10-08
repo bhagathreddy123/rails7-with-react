ecaf438b568376ccb4ab3014ba04fa4c45d4cd59
bhagathreddy123 <katta_reddy04@yahoo.com>
2023-10-08 08:31:46 -0700

    Implemented Like DisLike buttons using bootstrap5

--- app/javascript/react/src/components/QuestionDetail.jsx
+++ app/javascript/react/src/components/QuestionDetail.jsx
@@ -4,9 +4,14 @@ import * as ReactDom from 'react-dom'
 class QuestionDetail extends React.Component {
 	constructor(props){
 		super(props)
-		this.state = { likeCount: 0 }
+		this.state = {
+		  likeCount: 0,
+		  dislikeCount: 0 
+		}
+		
 
 		this.updateLikeCounter = this.updateLikeCounter.bind(this)
+		this.updateDisLikeCounter = this.updateDisLikeCounter.bind(this)
 	}
 
 	updateLikeCounter() {
@@ -16,18 +21,38 @@ class QuestionDetail extends React.Component {
 			}
 		})
 	}
+
+	updateDisLikeCounter() {
+		this.setState(function(state) {
+			return {
+				dislikeCount: state.dislikeCount + 1
+			}
+		})
+	}
+
+
 	render() {
 		return(
-
         <div className="card rounded-0 mt-3">
+          <div className="card-body">
 	        <h3 className="card-tile">{this.props.question.title}</h3>
 	        <p className="lead">
 	          <span className="badge bg-primary">{this.props.question.ans}</span>
 	        </p>
-	        <button className="btn btn-primary mt-1" onClick={ this.updateLikeCounter }>Like</button>
-	       { this.state.likeCount > 0 ?
-             <span className="badge bg-primary mt-1">{this.state.likeCount} </span> : ''
+	        <button type="button" className="btn btn-primary position-relative"  onClick={ this.updateLikeCounter }  style={{marginRight: 1+'em'}} >
+  				Like
+  				 { this.state.likeCount > 0 ?
+             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{this.state.likeCount} </span> : ''
 	       }
+			</button>
+
+			<button type="button" className="btn btn-primary position-relative"  onClick={ this.updateDisLikeCounter }>
+  				DisLike
+  				 { this.state.dislikeCount > 0 ?
+             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{this.state.dislikeCount} </span> : ''
+	       }
+			</button>
+	      </div>
 	    </div>
 	)
 	}
