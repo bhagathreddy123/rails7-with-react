b296aaa8ae088e4e4109dcf2cffe89c4aecf9314
bhagathreddy123 <katta_reddy04@yahoo.com>
2023-10-08 01:16:38 -0700

    using state hooks to display like count

--- app/javascript/react/src/components/QuestionDetail.jsx
+++ app/javascript/react/src/components/QuestionDetail.jsx
@@ -1,7 +1,11 @@
 import * as React from 'react'
+import { useState } from 'react'
 import * as ReactDom from 'react-dom'
 
 const QuestionDetail = (props) => {
+
+	const [likeCount, setLikeCount] = useState(0)
+
 	return(
 
         <div className="card rounded-0 mt-3">
@@ -9,6 +13,11 @@ const QuestionDetail = (props) => {
 	        <p className="lead">
 	          <span className="badge bg-primary">{props.question.ans}</span>
 	        </p>
+	        <button className="btn btn-primary mt-1" onClick={ () => 
+	      setLikeCount(likeCount + 1)}>Like</button>
+	       { likeCount > 0 ?
+             <span className="badge bg-primary mt-1">{likeCount} </span> : ''
+	       }
 	    </div>
 	)
 
