347b843493e2aa52999de369aa91f4610a632ccf
bhagathreddy123 <katta_reddy04@yahoo.com>
2023-10-08 04:58:17 -0700

    created Rails api end point for questions to fetch data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Question.create([
+
+    { 
+		title: "What is React?",
+		ans: "open-source front-end JavaScript library"
+	},
+
+	{ 
+		title: "What are the major features of React?",
+		ans: "JSX syntax"
+	},
+
+	{ 
+		title: "Why is DevTools not loading in Chrome for local files?",
+		ans: "open Chrome Extensions and check Allow access to file URLs"
+	},
+
+	{ 
+		title: "What is the difference between Element and Component?",
+		ans: "component can be declared in several different way"
+	},
+
+	{ 
+		title: "What are the advantages of React over Vue.js?",
+		ans: "Gives more flexibility in large apps developing"
+	}
+])
+
