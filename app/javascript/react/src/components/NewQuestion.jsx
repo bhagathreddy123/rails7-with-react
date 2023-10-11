af456dd10fb6893bc1a8e028fb41c28200f069ff
bhagathreddy123 <katta_reddy04@yahoo.com>
2023-10-10 19:20:23 -0700

    using single state and single handler function for form submition

--- app/javascript/react/src/components/NewQuestion.jsx
+++ app/javascript/react/src/components/NewQuestion.jsx
@@ -4,27 +4,25 @@ import * as ReactDOM from 'react-dom'
 
 const NewQuestion = () => {
 	const questionsTags = [
-      { label: 'Ruby', value: 0 },
-      { label: 'Rails', value: 1 },
-      { label: 'JavaScript', value: 2 },
-      { label: 'Typescript', value: 3 },
-      { label: 'React', value: 4 }
+      { label: 'Ruby', value: 'Ruby' },
+      { label: 'Rails', value: "Rails" },
+      { label: 'JavaScript', value: "JavaScript" },
+      { label: 'Typescript', value: "Typescript" },
+      { label: 'React', value: "React" }
 	]
 
-	const [title, setTitle] = useState('')
-	const [tag, setTag] = useState(questionsTags[0].value)
-
-	const handleTitleChange = (event) => {
-		setTitle(event.target.value)
-	}
-
-	const handleTagChange = (event) => {
-		setTag(event.target.value)
-	}
+	const [formField, setFormField] = useState({
+		title: ''
+		tag: questionsTags[0].value
+	})
 
 	const handleQuestionSubmit = (event) => {
       event.preventDefault();
-      console.log({title: title, tag: tag})
+      console.log(formField)
+    }
+
+    const handleFormFields = (event) => {
+      setFormField({ ...formField, [event.target.name]: event.target.value})
     }
 
 	return(
@@ -41,11 +39,11 @@ const NewQuestion = () => {
 			        <div className="form-group">
 			           <label className="form-label mt-3 mb-3"> Title</label>
 			           <input type="text" className="form-control form-control-lg rounded-0" value={title}
-			            onChange={event => handleTitleChange(event)} name="title" />
+			            onChange={event => handleFormFields(event)} name="title" />
 			        </div>
 			        <div className="form-group">
 			           <label className="form-label mt-3 mb-3"> Question Tag</label>
-			           <select className="form-select form-slect-lg rounded-0" value={tag} onChange={ event => handleTagChange(event}} name="tag">
+			           <select className="form-select form-slect-lg rounded-0" value={tag} onChange={ event => handleFormFields(event}} name="tag">
 			           	{ questionsTags.map(tag => (
 			           		<option key={tag.value} value={tag.value}>{tag.label}</option>
 			           		))}
